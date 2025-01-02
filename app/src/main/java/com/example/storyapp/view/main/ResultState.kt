cfdb707e7860fdfa8b373192e05329b7f8a13379
nadifahisma10 <129817146+nadifahisma10@users.noreply.github.com>
2025-01-02 11:29:52 +0700

    Delete app/src/main/java/com/example/storyapp/view/main/ResultState.kt

--- app/src/main/java/com/example/storyapp/view/main/ResultState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.storyapp.view.main
-
-sealed class ResultState <out R> {
-    data object Loading : ResultState<Nothing>()
-    data class Success<out T>(val data: T) : ResultState<T>()
-    data class Error(val error: String) : ResultState<Nothing>()
-}
